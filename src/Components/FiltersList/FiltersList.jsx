b2a3c667491a23dbf2e5de98f2575389223cd244
Nyanthie Mayang <nyanthie.mayang@student.moringaschool.com>
2022-07-31 20:21:08 +0300

    update

---
+++ src/Components/FiltersList/FiltersList.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import FilterItem from "../FilterItem";
+import styles from "./FiltersList.module.scss"
+
+export default class FiltersList extends Component {
+    getFilterJsx = (filter, index) => (
+        <div key={index}>
+            <FilterItem filter={filter} handleChecked={this.props.handleChecked} />
+        </div>
+    );
+
+    render() { 
+        return (
+            <section className={styles.filtersList}>
+                <h4>Filters</h4>
+                {this.props.filters.map(this.getFilterJsx)}
+            </section>
+        );
+    }
+}
